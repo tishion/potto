b80bfe67548f30915f5457e0cb9f4ae6f253ca23
Sheen (Shen) Tian <tishion@163.com>
2016-11-24 23:09:51 +0800

    Change the solution structure.

--- test/include/Ark/Ark_CLSID.h
+++
@@ -1,7 +0,0 @@
-#ifndef Ark_CLSID_H_
-#define Ark_CLSID_H_
-#pragma once
-const char* const CLSID_Sparrow = "0633e58e-11b0-4aa4-9ed7-bb147367fbe0";
-const char* const CLSID_Misc = "a85985be-7639-4d06-838d-e9d30d76785e";
-
-#endif // Ark_CLSID_H_
